79e7fa8a5a9d04965af0b9bf3611d0d7b4c78ef5
buldozer231 <56519162+buldozer231@users.noreply.github.com>
2020-05-25 17:44:38 +0300

    add export vars

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
+export DOCKER_REPO='docker.io'
+export DOCKER_USER='user'
+export DOCKERHUB_PASSWORD='password('
+export DOCKERHUB_EMAIL='email'
+export APP1='tomcat'
+export APP2='httpd'
+export APP3='nginx'
 set -ex
 
 cd ansible
-if [ "$1" == "app2" ]
+if [ "$1" != "" ]
 then
-    ansible-playbook -i hosts app2.yml
+    ansible-playbook -i hosts deploy.yml --tags $1
 else
-    ansible-playbook -i hosts app1.yml
+    echo "Select a tag"
+    ansible-playbook deploy.yml --list-tags
 fi
